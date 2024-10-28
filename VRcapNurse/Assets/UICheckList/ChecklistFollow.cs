a0d88f9deb2bfabf6d5fc688699f4657f941a636
Parag200 <91507000+Parag200@users.noreply.github.com>
2024-10-28 11:38:51 -0400

    Following Cube
    -cube follows front camera view
    -cube could have material of checklist

---
+++ VRcapNurse/Assets/UICheckList/ChecklistFollow.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChecklistFollow : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float distance = 3.0f;
+
+    private bool isCentered = false;
+
+    private void OnBecameInvisible()
+    {
+        isCentered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = FindTargetPosition();
+
+        MoveTowards(targetPosition);
+
+        if(ReachedPosition(targetPosition))
+        {
+            isCentered = true;
+        }
+    }
+
+    private Vector3 FindTargetPosition()
+    {
+        return cameraTransform.position + (cameraTransform.forward * distance);
+    }
+
+    private void MoveTowards (Vector3 targetPosition)
+    {
+        transform.position += (targetPosition - transform.position) * 0.025f;
+    }
+
+    private bool ReachedPosition(Vector3 targetPosition)
+    {
+        return Vector3.Distance(targetPosition, transform.position) < 0.1f;
+    }
+
+
+}
