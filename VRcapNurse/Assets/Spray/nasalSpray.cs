db343141b2c12710c3dbe6d21f93e12df9de327d
Parag200 <91507000+Parag200@users.noreply.github.com>
2024-11-03 13:53:54 -0500

    nitro sprays
    -nitro sprays when user is holding it and presses a
    (however we want a spray once not the spray conintus when holding a)

--- VRcapNurse/Assets/Spray/nasalSpray.cs
+++ VRcapNurse/Assets/Spray/nasalSpray.cs
@@ -1,21 +1,78 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class nasalSpray : MonoBehaviour
+public class NasalSpray : MonoBehaviour
 {
-
     public ParticleSystem spray;
 
     private XRGrabInteractable grabInteractable;
+    private bool isGrabbed = false;
 
-    private void Awake()
+    private enum ControllerSide
     {
+        Left_Controller,
+        Right_Controller
+    }
+
+    [SerializeField]
+    private ControllerSide m_controller;
+    private InputDeviceCharacteristics m_characteristics;
+
+    private void Start()
+    {
+        // Set characteristics based on controller side
+        m_characteristics = m_controller == ControllerSide.Left_Controller ?
+            InputDeviceCharacteristics.Left :
+            InputDeviceCharacteristics.Right;
+
         grabInteractable = GetComponent<XRGrabInteractable>();
         if (grabInteractable != null)
         {
-            grabInteractable.onSelectEntered.AddListener(PlayParticleSystem);
+            grabInteractable.onSelectEntered.AddListener(OnGrab);
+            grabInteractable.onSelectExited.AddListener(OnRelease);
+        }
+    }
+
+    private void Update()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(m_characteristics, devices);
+
+        if (devices.Count == 1)
+        {
+            CheckController(devices[0]);
+        }
+    }
+
+    private void CheckController(InputDevice device)
+    {
+        bool primaryButtonDown = false;
+        device.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonDown);
+
+        // Play the particle system if grabbed and the button is pressed
+        if (isGrabbed && primaryButtonDown)
+        {
+            PlayParticleSystem();
+        }
+    }
+
+    private void OnGrab(XRBaseInteractor interactor)
+    {
+        isGrabbed = true;
+    }
+
+    private void OnRelease(XRBaseInteractor interactor)
+    {
+        isGrabbed = false;
+    }
+
+    private void PlayParticleSystem()
+    {
+        if (spray != null)
+        {
+            spray.Play();
         }
     }
 
@@ -23,15 +80,8 @@ public class nasalSpray : MonoBehaviour
     {
         if (grabInteractable != null)
         {
-            grabInteractable.onSelectEntered.RemoveListener(PlayParticleSystem);
-        }
-    }
-
-    private void PlayParticleSystem(XRBaseInteractor interactor)
-    {
-        if (spray != null)
-        {
-            spray.Play();
+            grabInteractable.onSelectEntered.RemoveListener(OnGrab);
+            grabInteractable.onSelectExited.RemoveListener(OnRelease);
         }
     }
 }
